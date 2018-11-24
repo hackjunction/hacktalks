addf4c59e7b63292d2250aecb8ffe66689ff472b
Duc Toan Thanh <toanth@metropolia.fi>
2018-11-25 00:01:49 +0200

    Hack Talks 2018 retro

--- resources/js/script.js
+++ resources/js/script.js
@@ -14,8 +14,8 @@ $(document).ready(function() {
     
     
     /* Scroll on buttons */
-    $('.js--scroll-to-plans').click(function () {
-       $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000); 
+    $('.js--scroll-to-newsletter').click(function () {
+       $('html, body').animate({scrollTop: $('.js--section-newsletter').offset().top}, 1000); 
     });
     
     $('.js--scroll-to-start').click(function () {
